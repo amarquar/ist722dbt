1514751c506be1af9392fb06a71724728fb92246
Adam Marquardt <amarquar@syr.edu>
2025-01-06 08:50:26 +0000

    initialize dbt

--- Northwind/models/example/my_first_dbt_model.sql
+++ Northwind/models/example/my_first_dbt_model.sql
@@ -1,1 +1,22 @@
- 
+ {{ config(materialized='table') }}
+
+WITH dim_date as (
+    SELECT
+        datekey::int as datekey,
+        date,
+        year,
+        month,
+        quarter,
+        day,
+        dayofweek,
+        weekofyear,
+        dayofyear,
+        quartername,
+        monthname,
+        dayname,
+        weekday
+    FROM {{ source('conformed', 'DateDimension') }}
+)
+
+SELECT *
+FROM dim_date;
